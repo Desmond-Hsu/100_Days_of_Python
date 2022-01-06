d0bee4f5d429858efb202bb58fdf4bda11710673
Desmond-Hsu <derek97072002@gmail.com>
2022-01-06 21:48:29 +0800

    fulfill template

--- Day28_Pomodoro/main.py
+++ Day28_Pomodoro/main.py
@@ -20,22 +20,22 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224,bg=YELLOW)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file = "tomato.png")
-canvas.create_image(102,112,image=tomato_img)
+canvas.create_image(100,112,image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
-label_timer = Label(text="timer",fg=GREEN, bg=YELLOW, font=(FONT_NAME,40,"bold"))
+label_timer = Label(text="Timer",fg=GREEN, bg=YELLOW, font=(FONT_NAME,40))
 label_timer.grid(row=0, column=1)
 
-label_tick = Label(text="✔",fg=GREEN, bg=YELLOW, font=(FONT_NAME,25,"bold"))
-label_tick.grid(row=2, column=1)
+label_check = Label(text="✔",fg=GREEN, bg=YELLOW)
+label_check.grid(row=2, column=1)
 
-button_start = Button(text="start",bg="white")
+button_start = Button(text="Start",bg="white", highlightthickness=0)
 button_start.grid(row=2, column=0)
 
-button_end = Button(text="end",bg="white")
+button_end = Button(text="Reset",bg="white", highlightthickness=0)
 button_end.grid(row=2, column=2)
 
 window.mainloop()
